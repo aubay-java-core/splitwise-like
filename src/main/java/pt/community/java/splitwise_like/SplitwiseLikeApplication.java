e01ba6b33bdf96c59b862778a280d18fd11a70ba
Pedro Appel <appelpedroappel@gmail.com>
2025-02-04 13:18:35 +0000

    Inicial commit

---
+++ src/main/java/pt/community/java/splitwise_like/SplitwiseLikeApplication.java
@@ -0,0 +1,13 @@
+package pt.community.java.splitwise_like;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitwiseLikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitwiseLikeApplication.class, args);
+	}
+
+}
